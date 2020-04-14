62444bba33e3abdb568000aeb7d875494ff53b1c
nemtna <62949351+nemtna@users.noreply.github.com>
2020-04-13 21:25:02 -0700

    Initial Commit

---
+++ Guess the Flag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Guess the Flag
+//
+//  Created by Anthony Mendez on 4/13/20.
+//  Copyright © 2020 Anthony Mendez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
